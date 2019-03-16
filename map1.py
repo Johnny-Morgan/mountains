b1901c5772ea575b14f75e42244534262482abe8
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-03-16 19:29:40 +0000

    Added layers & 'fun facts'

--- map1.py
+++ map1.py
@@ -7,20 +7,28 @@ longitude = list(data["LON"])
 mountain_name = list(data["NAME"])
 height = list(data["HEIGHT"])
 prominence = list(data["PROM"])
+fact = list(data["FACT"])
 
-html = """<h4>Mountain information:</h4>
-Name: %s<br>
-Height: %sm<br>
-Prominence: %sm<br>
+html = """
+<b>Name:</b> %s<br>
+<b>Height:</b> %sm<br>
+<b>Prominence:</b> %sm<br>
+<b>Fun fact:</b> %s
 """
 #color based on height
 def color_picker(height):
-    if height < 600:
+    if height < 300:
+        return "lightgreen"
+    elif height < 500:
         return "green"
+    elif height < 600:
+        return "darkgreen"
     elif height < 700:
         return "orange"
-    else:
+    elif height < 800:
         return "red"
+    else:
+        return "darkred"
 
 # color based on arderins, arderin-begs & Vandeleur-Lynams
 # def color_picker(height, prominence):
@@ -33,13 +41,19 @@ def color_picker(height):
 
 map = folium.Map(location = [53.124, -6.350], zoom_start = 10)
 
-fg = folium.FeatureGroup(name = "My Map")
-for lat, lon, name, h, prom in zip(latitude, longitude, mountain_name, height, prominence):
-    color = "green"
-    if h > 700:
-        color = "red"
-    iframe = folium.IFrame(html=html % (name, str(h), str(prom)), width=200, height=100)
+fg_mountains = folium.FeatureGroup(name = "Mountains")
+fg_hills = folium.FeatureGroup(name = "Hills")
+for lat, lon, name, h, prom, f in zip(latitude, longitude, mountain_name, height, prominence, fact):
+    iframe = folium.IFrame(html=html % (name, str(h), str(prom), f), width=200, height=100)
     #fg.add_child(folium.Marker(location = [lat, lon], popup=folium.Popup(iframe), icon = folium.Icon(color = color_picker(h))))
-    fg.add_child(folium.CircleMarker(location = [lat, lon], popup=folium.Popup(iframe), radius = 7, fill = True, fill_color = color_picker(h), color = color_picker(h), fill_opacity = 0.7))
-map.add_child(fg)
+    if h > 500:
+        fg_mountains.add_child(folium.CircleMarker(location = [lat, lon], popup=folium.Popup(iframe),
+        radius = 9, fill = True, fill_color = color_picker(h), color = "black", fill_opacity = 0.8))
+    else:
+        fg_hills.add_child(folium.CircleMarker(location = [lat, lon], popup=folium.Popup(iframe),
+        radius = 9, fill = True, fill_color = color_picker(h), color = "black", fill_opacity = 0.8))
+
+map.add_child(fg_mountains)
+map.add_child(fg_hills)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
