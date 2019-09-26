0d9256b52050b553166f13b31db0e36f0b72f997
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-09-26 22:03:54 +0100

    Changed database to sqlite database

--- map1.py
+++ map1.py
@@ -1,13 +1,44 @@
 import folium
-import pandas
+import sqlite3
+import csv
+import os
 
-data = pandas.read_csv("mountains.txt")
-latitude = list(data["LAT"])
-longitude = list(data["LON"])
-mountain_name = list(data["NAME"])
-height = list(data["HEIGHT"])
-prominence = list(data["PROM"])
-fact = list(data["FACT"])
+
+con = sqlite3.connect("mountains.db")
+cur = con.cursor()
+
+##############################################################
+##### create sqlite database using existing txt database #####
+##############################################################
+
+# cur.execute("CREATE TABLE mountain (name TEXT, height REAL, prominence REAL, longitude REAL, latitude REAL, fact TEXT)")
+# with open('mountains.txt') as f:
+#     next(f) # skip header
+#     reader = csv.reader(f)
+#     data = []
+#     for row in reader:
+#         data.append(row)
+#
+# cur.executemany("INSERT INTO mountain (name, height, prominence, longitude, latitude, fact) VALUES (?, ?, ?, ?, ?, ?);", data)
+# con.commit()
+
+query = "SELECT * FROM mountain"
+mountain = cur.execute(query).fetchall()
+
+mountain_name = []
+mountain_height = []
+mountain_prom = []
+mountain_long = []
+mountain_lat = []
+mountain_fact = []
+
+for i in range(0, len(mountain)):
+    mountain_name.append(mountain[i][0])
+    mountain_height.append(mountain[i][1])
+    mountain_prom.append(mountain[i][2])
+    mountain_long.append(mountain[i][3])
+    mountain_lat.append(mountain[i][4])
+    mountain_fact.append(mountain[i][5])
 
 html = """
 <b>Name:</b> %s<br>
@@ -15,7 +46,9 @@ html = """
 <b>Prominence:</b> %sm<br>
 <b>Fun fact:</b> %s
 """
-#color based on height
+
+
+# color based on height
 def color_picker(height):
     if height < 300:
         return "lightgreen"
@@ -30,30 +63,25 @@ def color_picker(height):
     else:
         return "darkred"
 
-# color based on arderins, arderin-begs & Vandeleur-Lynams
-# def color_picker(height, prominence):
-#     if height >=500 and prominence < 30 and prominence > 15:
-#         return "red"
-#     elif height >= 600 and prominence >= 15 and prominence < 30:
-#         return "blue"
-#     elif height >= 600 and prominence >= 15:
-#         return "blue"
 
 map = folium.Map(location = [53.124, -5.650], zoom_start = 9)
 
 fg_mountains = folium.FeatureGroup(name = "Mountains")
 fg_hills = folium.FeatureGroup(name = "Hills")
-for lat, lon, name, h, prom, f in zip(latitude, longitude, mountain_name, height, prominence, fact):
-    iframe = folium.IFrame(html=html % (name, str(h), str(prom), f), width=200, height=100)
-    #fg.add_child(folium.Marker(location = [lat, lon], popup=folium.Popup(iframe), icon = folium.Icon(color = color_picker(h))))
+
+for lat, lon, name, h, prom, f in zip(mountain_lat, mountain_long, mountain_name, mountain_height, mountain_prom, mountain_fact):
+    iframe = folium.IFrame(html = html % (name, str(h), str(prom), f), width = 200, height = 100)
     if h > 500:
-        fg_mountains.add_child(folium.CircleMarker(location = [lat, lon], popup=folium.Popup(iframe),
+        fg_mountains.add_child(folium.CircleMarker(location = [lat, lon], popup = folium.Popup(iframe),
         radius = 9, fill = True, fill_color = color_picker(h), color = "black", fill_opacity = 0.8))
     else:
-        fg_hills.add_child(folium.CircleMarker(location = [lat, lon], popup=folium.Popup(iframe),
+        fg_hills.add_child(folium.CircleMarker(location = [lat, lon], popup = folium.Popup(iframe),
         radius = 9, fill = True, fill_color = color_picker(h), color = "black", fill_opacity = 0.8))
 
 map.add_child(fg_mountains)
 map.add_child(fg_hills)
 map.add_child(folium.LayerControl())
 map.save("Map1.html")
+
+# open map in browser
+os.system("start Map1.html")
